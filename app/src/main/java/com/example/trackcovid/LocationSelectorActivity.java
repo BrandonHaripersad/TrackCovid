7369e4cd61d7ca339bb02c412c0fd692b2455327
BrandonHaripersad <brandon.haripersad@gmail.com>
2020-11-15 17:30:32 -0500

    Improved spinners in Manual Selection, commits selection to sp then returns to Main and updates right away. Cleaned up interface.

--- app/src/main/java/com/example/trackcovid/LocationSelectorActivity.java
+++ app/src/main/java/com/example/trackcovid/LocationSelectorActivity.java
@@ -1,12 +1,14 @@
 package com.example.trackcovid;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,18 +16,24 @@ public class LocationSelectorActivity extends AppCompatActivity {
 
     Spinner countrySpinner;
     Spinner provinceSpinner;
+    SharedPreferences sp;
+    SharedPreferences.Editor spEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_selector);
-       initializePlaceholderSpinners();
+        initializePlaceholderSpinners();
+
+        sp = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+        spEditor = sp.edit();
 
         //TODO: OK button for selecting location
         final Button submitButton = findViewById(R.id.location_selector_submit_button);
         submitButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"TODO",Toast.LENGTH_SHORT).show();
+                Intent BackToMain = new Intent(LocationSelectorActivity.this, MainActivity.class);
+                startActivity((BackToMain));
             }
         });
 
@@ -38,19 +46,59 @@ public class LocationSelectorActivity extends AppCompatActivity {
         });
     }
 
-    private void initializePlaceholderSpinners(){
+    private void initializePlaceholderSpinners() {
         //Initialize Country Spinner
+
+        provinceSpinner = (Spinner) findViewById(R.id.province_spinner);
         countrySpinner = (Spinner) findViewById(R.id.country_spinner);
         ArrayAdapter<CharSequence> countryAdapter = ArrayAdapter.createFromResource(this,
                 R.array.test_country_array, android.R.layout.simple_spinner_item);
         countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         countrySpinner.setAdapter(countryAdapter);
 
+        countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                spEditor.putString("country", parent.getItemAtPosition(position).toString());
+                spEditor.commit();
+
+                if (parent.getItemAtPosition(position).equals("Canada")) {
+                    fillSpinner("Canada");
+
+                } else {
+                    fillSpinner("test_province_array");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+        public void fillSpinner(String name) {
+        if (name.equals("Canada")) {
+            ArrayAdapter<CharSequence> provinceAdapter = ArrayAdapter.createFromResource(this, R.array.Provinces, android.R.layout.simple_spinner_item);
+            provinceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            provinceSpinner.setAdapter(provinceAdapter);
+        } else {
+            ArrayAdapter<CharSequence> provinceAdapter = ArrayAdapter.createFromResource(this, R.array.test_province_array, android.R.layout.simple_spinner_item);
+            provinceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            provinceSpinner.setAdapter(provinceAdapter);
+        }
         //Initialize Province Spinner
-        provinceSpinner = (Spinner) findViewById(R.id.province_spinner);
-        ArrayAdapter<CharSequence> provinceAdapter = ArrayAdapter.createFromResource(this,
-                R.array.test_province_array, android.R.layout.simple_spinner_item);
-        provinceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        provinceSpinner.setAdapter(provinceAdapter);
+
+        provinceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                spEditor.putString("adminArea", parent.getItemAtPosition(position).toString());
+                spEditor.commit();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/trackcovid/MainActivity.java
+++ app/src/main/java/com/example/trackcovid/MainActivity.java
@@ -1,8 +1,5 @@
 package com.example.trackcovid;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,7 +7,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,7 +18,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.trackcovid.common.Country;
 import com.example.trackcovid.common.CaseTuple;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -40,6 +35,9 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Locale;
 import java.util.Map;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -78,14 +76,6 @@ public class MainActivity extends AppCompatActivity {
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Country Canada = snapshot.getValue(Country.class);
-                TextView country = findViewById(R.id.Country);
-                TextView population = findViewById(R.id.population);
-                TextView cases = findViewById(R.id.cases);
-
-                country.setText(Canada.getName().toString());
-                population.setText(Integer.toString(Canada.getPop()));
-                cases.setText(Integer.toString(Canada.getTotal_cases()));
 
             }
 
